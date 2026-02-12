aad4bafce5b02e97c2cb9f10b708e983ba3f71a4
AlexandraRivass <440039.joan23@fje.edu>
2026-02-12 23:26:02 +0100

    Estructura realizada

---
+++ scape/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    
+    <!-- Nombre del titulo de la pagina -->
+    <title>Home - Pagina</title>
+    <!-- Logo de la pagina inicial -->
+    <link rel="icon" href="">
+</head>
+<body>
+    <div class="container" class="FondoPagina">
+
+    </div>
+
+
+    <!--Metodo Post -> No se ve nada por la Url -->
+    <form action="" method="post" id="formulario">
+    <!-- Donde se ingresara el usuario -->
+        <label for="usuario">usuario:</label>
+        <input type="text" name="usuario" id="usuario">
+        <br/>
+        <button type="submit" id="enviar">Enviar</button>
+    </form>
+    
+<hr/>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script> 
+</body>
+</html>
